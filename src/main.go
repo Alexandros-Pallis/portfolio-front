426b2e4801fda49005ef2b58ea5ceceeb95dc0db
Alexandros-Pallis <alexanpall@gmail.com>
2024-04-12 19:16:01 +0300

    feat: added auto tls

--- src/main.go
+++ src/main.go
@@ -3,29 +3,32 @@ package main
 import (
 	"apallis/portfolio-front/renderer"
 	"apallis/portfolio-front/routes"
+	"log"
 	"net/http"
 
+	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.New()
-    router.Use(gin.Logger())
-    router.Use(gin.Recovery())
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
 	ginHTMLRenderer := router.HTMLRender
 	router.HTMLRender = &renderer.HTMLTemplRenderer{
 		FallbackHtmlRenderer: ginHTMLRenderer,
-    }
+	}
 	router.SetTrustedProxies(nil)
 	router.Static("/dist", "./dist")
 	router.Static("/icons", "./assets/icon")
-    router.StaticFile("/favicon.png", "./assets/favicon.png")
-    router.StaticFile("/bio.pdf", "./assets/bio.pdf")
+	router.StaticFile("/favicon.png", "./assets/favicon.png")
+	router.StaticFile("/bio.pdf", "./assets/bio.pdf")
 	routes.Init(router)
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	router.Run(":80")
+    log.Fatal(autotls.Run(router, "alexandrospallis.dev", "www.alexandrospallis.dev"))
+	// router.Run(":80")
 }
